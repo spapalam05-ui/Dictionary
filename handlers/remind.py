37e3400c9a3fe911507d68485e95c7e719ce615c
Нурсултан <zarmasevnursultan@gmail.com>
2026-07-22 03:23:51 +0500

    изменил Reminds

--- handlers/remind.py
+++ handlers/remind.py
@@ -6,26 +6,19 @@ from database import set_reminder
 
 router = Router()
 
-@router.message(Command("remind"))
-async def remind(message: Message, command: CommandObject):
-    print("REMIND WORKS")
 
 @router.message(Command("remind"))
 async def remind(message: Message, command: CommandObject):
+
+    print("ARGS =", command.args)
 
     if command.args is None:
-        await message.answer(
-            "Используй:\n/remind 20:00"
-        )
+        await message.answer("Используй:\n/remind 20:00")
         return
 
     remind_time = command.args.strip()
 
-    if len(remind_time) != 5 or ":" not in remind_time:
-        await message.answer(
-            "❌ Неверный формат.\nПример: /remind 20:00"
-        )
-        return
+    print("TIME =", remind_time)
 
     await set_reminder(
         message.from_user.id,
