375be200a3066f41be893edc4e6e364e9635fecd
Нурсултан <zarmasevnursultan@gmail.com>
2026-07-22 03:47:03 +0500

    Исправил remind

--- handlers/remind.py
+++ handlers/remind.py
@@ -1,7 +1,39 @@
 import asyncio
 from datetime import datetime
 
-from database import get_reminders
+from aiogram import Router
+from aiogram.filters import Command, CommandObject
+from aiogram.types import Message
+
+from database import set_reminder, get_reminders
+
+router = Router()
+
+
+@router.message(Command("remind"))
+async def remind(message: Message, command: CommandObject):
+    if command.args is None:
+        await message.answer(
+            "Используй:\n/remind 20:00"
+        )
+        return
+
+    remind_time = command.args.strip()
+
+    if len(remind_time) != 5 or ":" not in remind_time:
+        await message.answer(
+            "❌ Неверный формат.\nПример: /remind 20:00"
+        )
+        return
+
+    await set_reminder(
+        message.from_user.id,
+        remind_time
+    )
+
+    await message.answer(
+        f"✅ Напоминание установлено на {remind_time}"
+    )
 
 
 async def reminder_loop(bot):
@@ -15,7 +47,7 @@ async def reminder_loop(bot):
                 try:
                     await bot.send_message(
                         user_id,
-                        "📚 Время повторить английские слова!\n\nИспользуй /word 🇬🇧"
+                        "📚 Время повторить слова!\n\nНажми /word 🇬🇧"
                     )
                 except Exception as e:
                     print(e)
