5341e1c6e9380924c04e154330b5d12402540414
Нурсултан <zarmasevnursultan@gmail.com>
2026-07-25 05:53:56 +0500

    Checkkk

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,9 +1,12 @@
-from aiogram import Router, F
+from aiogram import Router
+from aiogram.filters import Command
 from aiogram.types import Message
+
+print("ADMIN LOADED")
 
 router = Router()
 
-@router.message(F.text == "/id")
+@router.message(Command("id"))
 async def my_id(message: Message):
     print("ID COMMAND")
     await message.answer(f"Твой ID: {message.from_user.id}")
