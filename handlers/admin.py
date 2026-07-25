8332f606ae0190d4904b90afc1b0f70d1ebcc689
Нурсултан <zarmasevnursultan@gmail.com>
2026-07-25 05:31:02 +0500

    again again

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,26 +1,9 @@
-from aiogram import Router
-from aiogram.filters import Command
+from aiogram import Router, F
 from aiogram.types import Message
-
-from database import get_users_count
 
 router = Router()
 
-
-@router.message(Command("id"))
+@router.message(F.text == "/id")
 async def my_id(message: Message):
     print("ID COMMAND")
     await message.answer(f"Твой ID: {message.from_user.id}")
-
-
-ADMIN_ID = 123456789
-
-
-@router.message(Command("users"))
-async def users(message: Message):
-    if message.from_user.id != ADMIN_ID:
-        return
-
-    count = await get_users_count()
-
-    await message.answer(f"👥 Пользователей: {count}")
