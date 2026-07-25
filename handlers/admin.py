9d3559c13dbb36a07b1e27e35092590493d6b737
Нурсултан <zarmasevnursultan@gmail.com>
2026-07-25 14:25:03 +0500

    change

--- handlers/admin.py
+++ handlers/admin.py
@@ -1,25 +1,9 @@
 from aiogram import Router
 from aiogram.types import Message
 
-from database import get_users_count
-
 router = Router()
-
-ADMIN_ID = 1203468356
-
 
 @router.message()
 async def admin_commands(message: Message):
-    print("Получено:", repr(message.text))
-
-    if message.text == "/id":
-        await message.answer(f"Твой ID: {message.from_user.id}")
-        return
-
-    if message.text == "/users":
-        if message.from_user.id != ADMIN_ID:
-            await message.answer("Нет доступа")
-            return
-
-        count = await get_users_count()
-        await message.answer(f"👥 Пользователей: {count}")
+    print("TEXT =", repr(message.text))
+    await message.answer("Получил: " + str(message.text))
