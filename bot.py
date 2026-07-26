2c7236fb29ce10ebd8d2d3bb7e42b3947d13e920
Нурсултан <zarmasevnursultan@gmail.com>
2026-07-26 21:58:55 +0500

    nj

--- bot.py
+++ bot.py
@@ -29,16 +29,19 @@ dp = Dispatcher()
 dp.include_router(admin_router)
 dp.include_router(start_router)
 
-# Кнопки главного меню ставим раньше общих обработчиков
 dp.include_router(premium_router)
-dp.include_router(categories_router)
 dp.include_router(help_router)
 dp.include_router(remind_router)
 dp.include_router(my_words_router)
+
+# Сначала обычные кнопки главного меню
 dp.include_router(word_router)
 dp.include_router(add_router)
 
-# Этот роутер лучше оставить последним
+# Категории ставим после них
+dp.include_router(categories_router)
+
+# Callback-обработчики последними
 dp.include_router(callbacks_router)
 
 
